ad78978bb05a6fe96521c966bdd5e183e6a38849
Nattanon Maneechote <hanry5546@gmail.com>
2022-03-07 20:39:50 +0700

    router products

--- app.js
+++ app.js
@@ -5,15 +5,28 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
+const productsRouter = require('./src/router/productsRouter');
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views");
+app.set("view engine" , "ejs");
+
 app.get("/",(req,res) => {
-    res.send("Hello World Update");
+    res.render('index',{
+        username:"FLIPFLOP",
+        customer: [
+            "AAA",
+            "BBB",
+            "CCC"
+        ]
+    });
 });
 
-app.listen(port,()=> {
-    debug("Listening on port " + chalk.green(port));
+app.use("/products",productsRouter)
+
+app.listen(PORT,()=> {
+    debug("Listening on port " + chalk.green(PORT));
 });
---
+++ src/router/productsRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const productsRouter = express.Router();
+const products = require('../data/products.json');
+
+productsRouter.route("/").get((req,res)=> {
+    res.render('products',{
+        products: products
+    });
+});
+
+productsRouter.route("/:id").get((req,res)=> {
+    const id = req.params.id;
+    res.render('product',{
+        item: products[id]
+    });
+});
+
+module.exports = productsRouter;
